312b10cdf90619476208bc34486b5925e9ff0025
Reked37 <dtruesdale37@gmail.com>
2023-11-20 13:12:53 -0600

    commented out config

--- server/app.py
+++ server/app.py
@@ -39,7 +39,7 @@ from flask import request, jsonify, make_response
 from flask_restful import Resource
 
 # Local imports
-from config import app, db, api
+# from config import app, db, api
 # Add your model imports
 from models import Player, Coach, Team, player_coach_association, Match
 
